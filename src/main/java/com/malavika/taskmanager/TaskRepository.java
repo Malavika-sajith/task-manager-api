eeaf3704d53afa6a3f15ab8addcfd0fb565685a4
Malavika Sajith <haibinu46@gmail.com>
2026-09-08 00:55:12 +0200

    Add Task entity , repository and Create/Read API endpoints

---
+++ src/main/java/com/malavika/taskmanager/TaskRepository.java
@@ -0,0 +1,7 @@
+package com.malavika.taskmanager;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.beans.JavaBean;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
