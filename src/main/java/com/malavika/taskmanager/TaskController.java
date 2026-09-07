394c650a252a625b803502c6e5a558be2c6c1747
Malavika Sajith <haibinu46@gmail.com>
2026-09-08 01:05:05 +0200

    Add delete Task API endpoint

--- src/main/java/com/malavika/taskmanager/TaskController.java
+++ src/main/java/com/malavika/taskmanager/TaskController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +25,10 @@ public class TaskController {
     public Task createTask(@RequestBody Task task){
         return taskRepository.save(task);
 
+    }
+    @DeleteMapping("/tasks/{id}")
+    public void deleteTask(@PathVariable Long id){
+        taskRepository.deleteById(id);
     }
 
 }
